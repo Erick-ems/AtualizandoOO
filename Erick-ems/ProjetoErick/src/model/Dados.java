33f5496acf875e14beca6e9d0d6895891fc89e14
Erick Santos <79948051+Erick-ems@users.noreply.github.com>
2023-01-26 20:18:45 -0300

    Add files via upload

---
+++ Erick-ems/ProjetoErick/src/model/Dados.java
@@ -0,0 +1,42 @@
+package model;
+
+public class Dados {
+
+	private Carro[] carros = new Carro[9999];
+	private int qtdCarros = 0;
+
+	public void fillWithSomeData() {
+
+		for (int i = 0; i < 5; i++) {
+			carros[i] = new Carro("A4", 2008, "seda", "esportivo", "900000", "190000", "Audi");
+		}
+		
+
+
+		qtdCarros = 5;
+
+	}
+
+	public Carro[] getCarros() {
+		return carros;
+	}
+
+	public void setCarros(Carro[] carros) {
+		this.carros = carros;
+	}
+
+	public void inserirEditarCarro(Carro c, int pos) {
+		this.carros[pos] = c;
+		if (pos == qtdCarros)
+			qtdCarros++;
+	}
+
+	public int getQtdCarros() {
+		return qtdCarros;
+	}
+
+	public void setQtdCarros(int qtdCarros) {
+		this.qtdCarros = qtdCarros;
+	}
+
+}
